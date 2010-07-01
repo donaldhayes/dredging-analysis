7ccf7597db33be18295dbe0385c745d38baf87de
maheshbabu.vattigunta <maheshbabu.vattigunta@c2a4d909-3516-c47f-7d8b-1112ed2e5277>
2010-07-01 23:56:41 +0000

    Interface design completed. Should implement the back-end functionality.

--- trunk/MyFrame.cpp
+++ trunk/MyFrame.cpp
@@ -7,7 +7,6 @@ BEGIN_EVENT_TABLE( MyFrame, wxFrame )
 	EVT_BUTTON( 1, OnRefresh ) 
 END_EVENT_TABLE()
 
-
 MyFrame:: MyFrame( const wxString& lTitle )
 		: mNotebook( NULL ),
 		  mDataPanelTab( NULL ),
@@ -19,19 +18,10 @@ MyFrame:: MyFrame( const wxString& lTitle )
 		  wxFrame( NULL, wxID_ANY, lTitle, wxDefaultPosition, wxSize(500,700), wxDEFAULT_FRAME_STYLE, wxString(_T("Frame")) )
 {
 
-	/*AddPageIndexes() ;
-	AddIndexOneSubpages() ;
-	AddIndexTwoSubpages() ;
-
-	IndexOne_MachinePage() ;
-	IndexOne_BucketPage() ;
-	IndexOne_DredgeAreaPage() ;
-
-	IndexTwo_SwingCharacteristicsPage() ;
-	IndexTwo_LateralBucketPlacementPage() ;
-	IndexTwo_BucketRowsPage() ;*/
-
+	AddPageIndexes() ;
 	AddButtons() ;
+
+	AddDataPageContent() ;
 
 }
 
@@ -39,98 +29,101 @@ void
 MyFrame :: AddPageIndexes()
 {
 
-	mNotebook = new wxNotebook( this, -1, wxDefaultPosition, wxSize(500,500),wxNB_FIXEDWIDTH, _T("Notebook") ) ;
+	mNotebookPanel = new wxPanel( this, wxID_ANY, wxDefaultPosition, wxSize( 500,500 ), wxDEFAULT, _T(" Notebook Panel") );
+	mNotebook = new wxNotebook( mNotebookPanel, -1, wxDefaultPosition, wxSize(500,500),wxNB_FIXEDWIDTH, _T("Notebook") ) ;
 	mDataPanelTab = new wxPanel( mNotebook, wxID_ANY ) ;
 	mResultsPanelTab = new wxPanel( mNotebook, wxID_ANY ) ;
-	mNotebook->AddPage( mDataPanelTab, _T(" Data "),true, 0 ) ;
-	mNotebook->AddPage( mResultsPanelTab, _T(" Results " ), false, 1 ) ;
-
+	mNotebook->AddPage( mDataPanelTab, _T(" Data "),true, 1 ) ;
+	mNotebook->AddPage( mResultsPanelTab, _T(" Results " ), false, 2 ) ;
+	
 }
-
-void
-MyFrame :: AddIndexOneSubpages()
-{
-}
-
-void
-MyFrame :: AddIndexTwoSubpages()
-{
-}
-
-void
-MyFrame :: IndexOne_BucketPage()
-{
-	/*
-		std::cout <<"\n\n Enter Bucket Data \n " ;
-		std::cout <<"-------------------\n" ;
-
-		std::cout <<" \t Bucket Width:  " ; 
-		std::cin >> mBucketWidth ;
-		std::cout <<" \t Bucket Length:  " ; 
-		std::cin >> mBucketLength ;
-		std::cout <<" \t Depth of Penetration:  " ; 
-		std::cin >> mPenetrationDepth ;
-	*/
-}
-
-void
-MyFrame :: IndexOne_DredgeAreaPage()
-{
-	/*
-	std::cout <<"\n\n Enter Dredge Area Data \n " ;
-	std::cout <<"----------------------------\n" ;
-	std::cout <<" \t Area Width:   " ; 
-	std::cin >> mDredgeWidth ;
-	std::cout <<" \t Area Length:  " ; 
-	std::cin >> mDredgeLength ;
-	std::cout <<" \t Lateral Bucket Overlap at the Centre Line:  " ; 
-	std::cin >> mLateralOverlap ;
-	std::cout <<" \t Longitudinal Bucket Over
-	*/
-}
-
-void
-MyFrame :: IndexOne_MachinePage()
-{
-	/*
-	std::cout <<"\n\n Enter Dredging Machine Data \n" ;
-	std::cout <<" ------------------------------- \n" ;
-
-	std::cout <<"\t Boom Length:  " ;
-	std::cin >> mBoomLength ;
-	std::cout <<"\t Maximum Dredging Width:  " ;
-	std::cin >> mMaxDredgeWidth ;
-	std::cout <<"\t Minimum Dredge Reach:  " ;
-	std::cin >> mMinDredgeReach ;
-	std::cout <<"\t Maximum Dredge Reach:  " ;
-	std::cin >> mMaxDredgeReach ;
-	*/
-}
-
-void
-MyFrame :: IndexTwo_SwingCharacteristicsPage()
-{
-}
-
-void
-MyFrame :: IndexTwo_LateralBucketPlacementPage()
-{
-}
-
-void
-MyFrame :: IndexTwo_BucketRowsPage()
-{
-}
-
 
 void
 MyFrame :: AddButtons()
 {
 
-		mButtonPanel   = new wxPanel( this, wxID_ANY, wxPoint(0,500), wxSize(500,200), wxTAB_TRAVERSAL, wxString(_T("Panel")) ) ;
-		mSubmitButton  = new wxButton( mButtonPanel, 0, _T(" Submit "), wxPoint(100,600), wxDefaultSize ) ;
-		mRefreshButton = new wxButton( mButtonPanel, 1, _T(" Refresh"), wxPoint(300,600), wxDefaultSize ) ;	
+	mButtonPanel   = new wxPanel( this, wxID_ANY, wxPoint(0,500), wxSize(500,200), wxTAB_TRAVERSAL, wxString(_T("Panel")) ) ;
+	mSubmitButton  = new wxButton( mButtonPanel, 0, _T(" Submit "), wxPoint(100,100), wxDefaultSize ) ;
+	mRefreshButton = new wxButton( mButtonPanel, 1, _T(" Refresh"), wxPoint(300,100), wxDefaultSize ) ;	
 
+}
+
+void
+MyFrame :: AddDataPageContent()
+{
+	
+	DredgeAreaData() ;
+	MachineData() ;
+	BucketData() ;
+
+}
+
+void
+MyFrame :: DredgeAreaData()
+{
+	
+	mDredgeAreaData						= new wxStaticBox ( mDataPanelTab, wxID_ANY, _T("&Dredge Area"), wxDefaultPosition, wxSize(475,170), 0, _T("Dredge Area Data Boundary") ) ;
+	
+	mST_DredgeArea_Length				= new wxStaticText( mDataPanelTab, wxID_ANY, _T("Length"), wxPoint(50,50), wxDefaultSize, 0, _T("Static Dredge Area Length Label") ) ;
+	mST_DredgeArea_Width				= new wxStaticText( mDataPanelTab, wxID_ANY, _T("Width"),  wxPoint(50,120),wxDefaultSize, 0, _T("Static Dredge Area Width Label" ) ) ;
+	mST_DredgeArea_LateralOverlap		= new wxStaticText( mDataPanelTab, wxID_ANY, _T("Lateral Overlap"), wxPoint(250,50), wxDefaultSize, 0, _T("Static Dredge Area Lateral Overlap Label") );
+	mST_DredgeArea_LongitudinalOverlap	= new wxStaticText( mDataPanelTab, wxID_ANY, _T("Longitudinal Overlap"), wxPoint(250,120), wxDefaultSize, 0, _T("Static Dredge Area Longitudinal Overlap Label") );
+
+	mDredgeArea_Length					= new wxTextCtrl( mDataPanelTab, wxID_ANY, wxEmptyString, wxPoint(100,50), wxDefaultSize, 0, wxDefaultValidator, _T("Text Box To Enter Dredge Area Length") ) ;
+	mDredgeArea_Width					= new wxTextCtrl( mDataPanelTab, wxID_ANY, wxEmptyString, wxPoint(100,120), wxDefaultSize, 0, wxDefaultValidator, _T("Text Box To Enter Dredge Area Width") ) ;
+	mDredgeArea_LateralOverlap			= new wxTextCtrl( mDataPanelTab, wxID_ANY, wxEmptyString, wxPoint(350,50), wxDefaultSize, 0, wxDefaultValidator, _T("Text Box To Enter Dredge Area Lateral Overlap") ) ;
+	mDredgeArea_LongitudinalOverlap		= new wxTextCtrl( mDataPanelTab, wxID_ANY, wxEmptyString, wxPoint(350,120), wxDefaultSize, 0, wxDefaultValidator, _T("Text Box To Enter Dredge Area Longitudinal Overlap") ) ;
+
+}
+
+void
+MyFrame :: MachineData()
+{
+	
+	mMachineData						= new wxStaticBox ( mDataPanelTab, wxID_ANY, _T("&Machine"), wxPoint(0,175), wxSize(475,170), 0, _T("Machine Data Boundary") ) ;
+
+	mST_Machine_BoomLength				= new wxStaticText( mDataPanelTab, wxID_ANY, _T("Boom Length"), wxPoint(50,230), wxDefaultSize, 0, _T("Machine Boom Length Static Label") );
+	mST_Machine_MaxDredgeWidth			= new wxStaticText( mDataPanelTab, wxID_ANY, _T("Max. Dredge Width"), wxPoint(50,300), wxDefaultSize, 0, _T("Machine Max. Dredge Width Static Label") );
+	mST_Machine_MinDredgeReach			= new wxStaticText( mDataPanelTab, wxID_ANY, _T("Min. Dredge Reach"), wxPoint(250,230), wxDefaultSize, 0, _T("Machine Min. Dredge Reach Static Label") );
+	mST_Machine_MaxDredgeReach			= new wxStaticText( mDataPanelTab, wxID_ANY, _T("Max. Dredge Reach"), wxPoint(250,300), wxDefaultSize, 0, _T("Machine Max. Dredge Reach Static Label") );
+
+	mMachine_BoomLength					= new wxTextCtrl( mDataPanelTab, wxID_ANY, wxEmptyString, wxPoint(150,228), wxSize(80,23), 0, wxDefaultValidator, _T("Text Box To Enter Machine Boom Length") ) ;
+	mMachine_MaxDredgeWidth				= new wxTextCtrl( mDataPanelTab, wxID_ANY, wxEmptyString, wxPoint(150,298), wxSize(80,23), 0, wxDefaultValidator, _T("Text Box To Enter Max. Machine Width") ) ;
+	mMachine_MinDredgeReach				= new wxTextCtrl( mDataPanelTab, wxID_ANY, wxEmptyString, wxPoint(350,228), wxSize(80,23), 0, wxDefaultValidator, _T("Text Box To Enter Min. Dredge Reach") ) ;
+	mMachine_MaxDredgeReach				= new wxTextCtrl( mDataPanelTab, wxID_ANY, wxEmptyString, wxPoint(350,298), wxSize(80,23), 0, wxDefaultValidator, _T("Text Box To Enter Max. Dredge Reach") ) ;
+
+}
+
+void
+MyFrame :: BucketData()
+{
+	
+	mBucketData							= new wxStaticBox ( mDataPanelTab, wxID_ANY, _T("&Bucket"), wxPoint(0,350), wxSize(475,120), 0, _T("Bucket Data Boundary") ) ;
+
+	mST_Bucket_Length					= new wxStaticText( mDataPanelTab, wxID_ANY, _T("Length"), wxPoint(50,405), wxDefaultSize, 0, _T("Bucket Length Static Label") );
+	mST_Bucket_Width					= new wxStaticText( mDataPanelTab, wxID_ANY, _T("Width"), wxPoint(180,405), wxDefaultSize, 0, _T("Bucket Width Static Label") );
+	mST_Bucket_PenetrationDepth			= new wxStaticText( mDataPanelTab, wxID_ANY, _T("Penetration Depth"), wxPoint(300,405), wxDefaultSize, 0, _T("Bucket Penetration Depth Static Label") );
+
+	mBucket_Length						= new wxTextCtrl( mDataPanelTab, wxID_ANY, wxEmptyString, wxPoint(100,403), wxSize(50,23), 0, wxDefaultValidator, _T("Text Box To Enter Bucket Length") ) ;
+	mBucket_Width						= new wxTextCtrl( mDataPanelTab, wxID_ANY, wxEmptyString, wxPoint(220,403), wxSize(50,23), 0, wxDefaultValidator, _T("Text Box To Enter Bucket Width") ) ;
+	mBucket_PenetrationDepth			= new wxTextCtrl( mDataPanelTab, wxID_ANY, wxEmptyString, wxPoint(400,403), wxSize(50,23), 0, wxDefaultValidator, _T("Text Box To Enter Bucket Penetration Depth") ) ;
+
+}
+
+
+void
+MyFrame :: OnSubmit( wxCommandEvent& SubmitEvent )
+{
+	Calculate_SwingCharacteristics() ;
+	Calculate_LateralBucketPlacements() ;
+	Calculate_BucketRows() ;
+}
+
+
+void
+MyFrame :: OnRefresh( wxCommandEvent& RefreshEvent )
+{
+	
 }
 
 
@@ -200,14 +193,3 @@ MyFrame :: Calculate_BucketRows()
 	*/
 }
 
-void
-MyFrame :: OnSubmit( wxCommandEvent& SubmitEvent )
-{
-	std::cout <<" Submit button clicked " << std::endl ;
-}
-
-void
-MyFrame :: OnRefresh( wxCommandEvent& RefreshEvent )
-{
-	std::cout <<" Refresh button clicked" << std::endl ;
-}
--- trunk/MyFrame.h
+++ trunk/MyFrame.h
@@ -11,18 +11,12 @@ class MyFrame : public wxFrame
 		
 		MyFrame(const wxString& title) ;
 		void AddPageIndexes() ;
-		void AddIndexOneSubpages() ;
-		void AddIndexTwoSubpages() ;
-
-		void IndexOne_MachinePage() ;
-		void IndexOne_BucketPage() ;
-		void IndexOne_DredgeAreaPage() ;
-
-		void IndexTwo_SwingCharacteristicsPage() ;
-		void IndexTwo_LateralBucketPlacementPage() ;
-		void IndexTwo_BucketRowsPage() ;
-
 		void AddButtons() ;
+		
+		void AddDataPageContent() ;
+		void MachineData() ;
+		void BucketData() ;
+		void DredgeAreaData() ;
 
 		void Calculate_SwingCharacteristics() ;
 		void Calculate_LateralBucketPlacements() ;
@@ -33,12 +27,24 @@ class MyFrame : public wxFrame
 
 	private:
 
-		wxPanel *mButtonPanel ;
+		wxPanel *mButtonPanel, *mNotebookPanel ;
 		wxPanel *mDataPanelTab, *mResultsPanelTab ;
-		wxNotebook *mNotebook ;
+		wxPanel *mFirstChildMachinePanel ;
+		wxNotebook *mNotebook, *mFirstChildNotebook ;
 		wxButton *mSubmitButton, *mRefreshButton ;
 
+		wxStaticBox *mMachineData, *mBucketData, *mDredgeAreaData ;
+
+		wxStaticText *mST_Machine_BoomLength, *mST_Machine_MaxDredgeWidth, *mST_Machine_MinDredgeReach, *mST_Machine_MaxDredgeReach ;
+		wxStaticText *mST_Bucket_Width, *mST_Bucket_Length, *mST_Bucket_PenetrationDepth ;
+		wxStaticText *mST_DredgeArea_Width, *mST_DredgeArea_Length, *mST_DredgeArea_LateralOverlap, *mST_DredgeArea_LongitudinalOverlap ;
+
+		wxTextCtrl *mMachine_BoomLength, *mMachine_MaxDredgeWidth, *mMachine_MinDredgeReach, *mMachine_MaxDredgeReach ;
+		wxTextCtrl *mBucket_Width, *mBucket_Length, *mBucket_PenetrationDepth ;
+		wxTextCtrl *mDredgeArea_Width, *mDredgeArea_Length, *mDredgeArea_LateralOverlap, *mDredgeArea_LongitudinalOverlap ;
+
 	DECLARE_EVENT_TABLE()
+
 };
 
 
